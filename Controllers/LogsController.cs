2aa624d21bd99938b101f91b3d900a66a36802ff
Nick A <nakursk@gmail.com>
2025-01-16 18:56:02 +0300

    финиш

---
+++ Controllers/LogsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcStartApp.Models.DB;
+using MvcStartApp.Models.Repository;
+
+namespace MvcStartApp.Controllers
+{
+    public class LogsController : Controller
+    {
+        private readonly IRequestRepository _repo;
+        private readonly BlogContext _context;
+
+        // ReSharper disable once ConvertToPrimaryConstructor
+        public LogsController(IRequestRepository repo, BlogContext context)
+        {
+            _repo = repo;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var logs = (await _repo.GetRequests()).OrderBy(o => o.Date);
+            return View(logs);
+        }
+    }
+}
